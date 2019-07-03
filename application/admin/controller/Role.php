88fa8c6e326febbb27a2fc138919a1c266a2a632
付帅 <you@example.com>
2019-07-03 21:36:08 +0800

    权限增删改查

--- application/admin/controller/Role.php
+++ application/admin/controller/Role.php
@@ -3,58 +3,39 @@ namespace app\admin\controller;
 use gmars\rbac\Rbac;
 use Request;
 use think\Db;
-use app\admin\model\Permission as PermissionModel;
-use app\admin\model\Permissioncate as PermissioncateModel;
-class Permission extends Common
+class Role extends Common
 {
 	public function index()
     {
-      $arr=Db::table('permission_category')->select();
-      $this->assign('arr',$arr);
+     
     	return $this->fetch();
     }
     //展示
    public function show()
   {
-    $arr=Db::query("select p.id,p.name,p.path,p.description, p.category_id,pc.id as pc_id,pc.name as pc_name  FROM permission as p left join permission_category as pc on p.category_id=pc.id");
+   //  $arr=Db::query("select p.id,p.name,p.path,p.description, p.category_id,pc.id as pc_id,pc.name as pc_name  FROM permission as p left join permission_category as pc on p.category_id=pc.id");
 
-     // $arr=Db::query("select * from permission as p inner join permission_category as pc on p.category_id=pc.id");
+   //   // $arr=Db::query("select * from permission as p inner join permission_category as pc on p.category_id=pc.id");
     
-  	$res=['code'=>'0','status'=>'ok','data'=>$arr];
-  	echo $json=json_encode($res);
+  	// $res=['code'=>'0','status'=>'ok','data'=>$arr];
+  	// echo $json=json_encode($res);
 	
   }
-  //添加
-  public function addAction(){
-      $data=Request::post('');
 
-      $validate = new \app\admin\validate\Permission;
-        if (!$validate->check($data)) {
-           $res=['code'=>'1','status'=>'error','data'=>$validate->getError()];
-            echo json_encode($res);
-            die;
-        }
-        unset($data['__token__']);
-      $rbac = new Rbac();
-      $arr=$rbac->getPermission([['name', '=', $data['name']]]);
-      $path=$rbac->getPermission([['path', '=', $data['path']]]);
-      // $sql=Db::query("select * from permission where name='$name' or path='$path' ");
-      if (empty($arr)&&empty($psth)) {
-        $rbac->createPermission([
-        'name' => $data['name'],
-        'description' => $data['description'],
-        'status' => 1,
-        'type' => 1,
-        'category_id' => $data['cateid'],
-        'path' => $data['path'],
-        ]);
-        $res=['code'=>'0','status'=>'ok','data'=>'添加成功'];
-        echo json_encode($res);
-        die;
-      }else{
-        $res=['code'=>'2','status'=>'error','data'=>'名称和路径不能重复'];
-        echo json_encode($res);
-      }
+  public function add(){
+  	
+ 		return $this->fetch();
+  }
+  public function roleshow(){
+  		$arr=Db::query("select p.id,p.name,p.path,p.description, p.category_id,pc.id as pc_id,pc.name as pc_name  FROM permission as p left join permission_category as pc on p.category_id=pc.id");
+  		
+  		$sql=[];
+  		foreach ($arr as $key => $value) {
+  			$sql[$value['pc_name']][]=$value;
+  		}
+  		$res=['code'=>'0','status'=>'ok','data'=>$sql];
+
+  		echo $json=json_encode($res);
   }
   //删除
   public function pc_delete(){
